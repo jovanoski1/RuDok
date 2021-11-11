e65e087df1441b8196c8c93cdfa777aac2127320
jovanoski1 <mihailojovanoski14@gmail.com>
2021-11-11 17:39:30 +0100

    move IPublisher methodes to RuNode

--- src/rudok/model/tree/RuNode.java
+++ src/rudok/model/tree/RuNode.java
@@ -20,6 +20,34 @@ public abstract class RuNode implements IPublisher {
         this.notifySubscribers(ime);
     }
 
+    @Override
+    public void addSubscriber(ISubscriber sub) {
+        if(sub == null)
+            return;
+        if(this.getSubscribers() ==null)
+            this.setSubscribers(new ArrayList<>());
+        if(this.getSubscribers().contains(sub))
+            return;
+        this.getSubscribers().add(sub);
+    }
+
+    @Override
+    public void removeSubscriber(ISubscriber sub) {
+        if(sub == null || this.getSubscribers() == null || !this.getSubscribers().contains(sub))
+            return;
+        this.getSubscribers().remove(sub);
+    }
+
+    @Override
+    public void notifySubscribers(Object notification) {
+        if(notification == null || this.getSubscribers() == null || this.getSubscribers().isEmpty())
+            return;
+
+        for(ISubscriber listener : getSubscribers()){
+            listener.update(notification);
+        }
+    }
+
     public RuNode getParent() {
         return parent;
     }
--- src/rudok/model/workspace/Slide.java
+++ src/rudok/model/workspace/Slide.java
@@ -22,32 +22,4 @@ public class Slide extends RuNode {
     public void setRedniBroj(int redniBroj) {
         this.redniBroj = redniBroj;
     }
-
-    @Override
-    public void addSubscriber(ISubscriber sub) {
-        if(sub == null)
-            return;
-        if(this.getSubscribers() ==null)
-            this.setSubscribers(new ArrayList<>());
-        if(this.getSubscribers().contains(sub))
-            return;
-        this.getSubscribers().add(sub);
-    }
-
-    @Override
-    public void removeSubscriber(ISubscriber sub) {
-        if(sub == null || this.getSubscribers() == null || !this.getSubscribers().contains(sub))
-            return;
-        this.getSubscribers().remove(sub);
-    }
-
-    @Override
-    public void notifySubscribers(Object notification) {
-        if(notification == null || this.getSubscribers() == null || this.getSubscribers().isEmpty())
-            return;
-
-        for(ISubscriber listener : getSubscribers()){
-            listener.update(notification);
-        }
-    }
 }
