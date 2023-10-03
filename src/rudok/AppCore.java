6e53653706e371e82d4964aff4062b027d7ddea7
jovanoski1 <mihailjovanosk14@gmail.com>
2023-10-03 09:31:37 +0200

    asd

--- src/rudok/AppCore.java
+++ src/rudok/AppCore.java
@@ -5,6 +5,7 @@ import rudok.view.MainFrame;
 
 public class AppCore {
     public static void main(String[] args) {
+        int a = 5;
         MainFrame b = MainFrame.getInstance();
         b.setLocationRelativeTo(null);
         b.setVisible(true);
